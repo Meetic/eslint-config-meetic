6eb36182c8f9fa3bb7dd614669e7a44717b06da5
olegskl <sklyanchuk@gmail.com>
2015-07-03 14:55:56 +0200

    feat(rules/legacy): raise max-len to 120 chars

--- rules/legacy.js
+++ rules/legacy.js
@@ -13,7 +13,7 @@ module.exports = {
     // Specify the maximum depth that blocks can be nested (off by default)
     'max-depth': [2, 4],
     // Specify the maximum length of a line in your program (off by default)
-    'max-len': [2, 80, 2],
+    'max-len': [2, 120, 2],
     // Limits the number of parameters that can be used in the function declaration. (off by default)
     'max-params': [2, 10],
     // Specify the maximum number of statement allowed in a function (off by default)
