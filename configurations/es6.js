af037b21a0b491bf060f8eab94e3ecc837d67a94
b.delamarre <b.delamarre@meetic-corp.com>
2016-02-22 18:16:34 +0100

    feat(config): Enable ES6 features in es6 rules

--- configurations/es6.js
+++ configurations/es6.js
@@ -8,7 +8,15 @@
 var deepExtend = require('deep-extend');
 
 module.exports = deepExtend(
-  {},
+  {
+    env: {
+      es6: true
+    },
+    parserOptions: {
+      ecmaVersion: 6,
+      sourceType: 'module'
+    }
+  },
   require('./es5.js'),
   require('../rules/ecmascript-6.js'),
   require('../rules/best-practices-es6.js'),
